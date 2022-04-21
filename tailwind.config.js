ccf3190e208ca11340df5453de66fd48d5ba4d42
nacen <nacen.dev@gmail.com>
2022-04-21 18:55:17 +0800

    Add customBlack color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlack: "#14213d",
+      },
+    },
   },
   plugins: [],
 };
