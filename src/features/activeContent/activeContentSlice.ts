d6fbead7f3629cd4776f0b86d737ab7bf5195b4a
nacen <nacen.dev@gmail.com>
2022-04-22 19:21:11 +0800

    Create activeContentSlice

---
+++ src/features/activeContent/activeContentSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+export interface ActiveContent {
+  id: string;
+  type: "Project";
+}
+
+const initialState: ActiveContent = {
+  id: "Home",
+  type: "Project",
+};
+
+export const activeContentSlice = createSlice({
+  name: "activeContent",
+  initialState: initialState,
+  reducers: {
+    changeActiveContent: (state, action: PayloadAction<ActiveContent>) => {
+      state = action.payload;
+      return state;
+    },
+  },
+});
+
+export const { changeActiveContent } = activeContentSlice.actions;
+
+export const selectActiveContent = (state: RootState) => state.activeContent;
+
+export default activeContentSlice.reducer;
