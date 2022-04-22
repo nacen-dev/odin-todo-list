9d3d535804c4a954e3dd387f351852bd2ee2aad9
nacen <nacen.dev@gmail.com>
2022-04-22 22:02:48 +0800

    Create Modal Component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { MdClose } from "react-icons/md";
+
+type Props = {
+  open: boolean;
+  children: React.ReactNode;
+  onClose: () => void;
+  className?: string;
+};
+
+const Modal = ({ children, open, onClose, className }: Props) => {
+  if (!open) return null;
+
+  let portalDiv = document.getElementById("portal");
+
+  const handleButtonClose = (event: React.MouseEvent<SVGElement>) => {
+    event.stopPropagation();
+    onClose();
+  };
+
+  return portalDiv
+    ? ReactDOM.createPortal(
+        <>
+          <div
+            className="fixed top-0 left-0 right-0 bottom-0 bg-black-rgba z-[1000]"
+            onClick={onClose}
+          />
+          <div
+            className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5 z-[1000] ${className}`}
+          >
+            <MdClose
+              onClick={handleButtonClose}
+              className="absolute top-4 right-4"
+            />
+            {children}
+          </div>
+        </>,
+        portalDiv
+      )
+    : null;
+};
+
+export { Modal };
