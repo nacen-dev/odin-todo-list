a8f55b315774ad07b7ff0d1104a687c812056a29
nacen <nacen.dev@gmail.com>
2022-04-25 18:27:19 +0800

    Use EditTodoModal to edit todo's

--- src/features/ProjectList/Todo.tsx
+++ src/features/ProjectList/Todo.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
 import { useAppDispatch } from "../../app/hooks";
-import { deleteTodo, toggleCompleteTodo } from "./projectListSlice";
+import { EditTodoModal } from "./EditTodoModal";
+import { deleteTodo, toggleCompleteTodo, editTodo } from "./projectListSlice";
 
 type TodoPriority = "low" | "medium" | "high";
 
@@ -17,6 +18,10 @@ export interface ITodo {
 }
 
 const Todo = (props: ITodo) => {
+  const { completed, dueDate, id, priority, title } = props;
+
+  const [open, setOpen] = useState(false);
+
   const dispatch = useAppDispatch();
 
   const handleDelete = () => {
@@ -27,41 +32,58 @@ const Todo = (props: ITodo) => {
     dispatch(toggleCompleteTodo(props));
   };
 
+  const handleEditTodo = (todo: ITodo) => {
+    dispatch(editTodo(todo));
+  };
+
   const priorityColor = (priority: TodoPriority) => {
     if (priority === "low") return "border-l-green-500";
     else if (priority === "medium") return "border-l-yellow-500";
     else if (priority === "high") return "border-l-red-500";
   };
 
-  const { completed, dueDate, id, priority, title } = props;
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
 
   return (
-    <div
-      className={`flex p-4 items-center gap-2 bg-slate-100 rounded-md rounded-l-none border-l-4 ${priorityColor(
-        priority
-      )}`}
-    >
-      <input
-        type="checkbox"
-        name="completed"
-        id={id}
-        checked={completed}
-        onChange={handleToggleCompleteTodo}
-      />
-      <p className="text-xl flex-1">{title}</p>
-      <div className="flex items-center gap-3">
-        <button className="text-sky-700 border-2 border-sky-700 rounded px-3 text-lg hover:bg-sky-700 hover:text-white">
-          Details
-        </button>
-        <p className="text-lg">{dueDate}</p>
-        <button className="edit-todo">
-          <MdEdit className="text-sky-700 text-xl" />
-        </button>
-        <button className="delete-todo" onClick={handleDelete}>
-          <MdDelete className="text-red-600 text-xl" />
-        </button>
+    <>
+      <div
+        className={`flex p-4 items-center gap-2 bg-slate-100 rounded-md rounded-l-none border-l-4 ${priorityColor(
+          priority
+        )}`}
+      >
+        <input
+          type="checkbox"
+          name="completed"
+          id={id}
+          checked={completed}
+          onChange={handleToggleCompleteTodo}
+        />
+        <p className="text-xl flex-1">{title}</p>
+        <div className="flex items-center gap-3">
+          <button className="text-sky-700 border-2 border-sky-700 rounded px-3 text-lg hover:bg-sky-700 hover:text-white">
+            Details
+          </button>
+          <p className="text-lg">{dueDate}</p>
+          <button className="edit-todo" onClick={handleOpenModal}>
+            <MdEdit className="text-sky-700 text-xl" />
+          </button>
+          <button className="delete-todo" onClick={handleDelete}>
+            <MdDelete className="text-red-600 text-xl" />
+          </button>
+        </div>
       </div>
-    </div>
+      <EditTodoModal
+        onClose={handleCloseModal}
+        open={open}
+        Todo={props}
+        editTodo={handleEditTodo}
+      />
+    </>
   );
 };
 
