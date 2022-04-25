ed44be45b24e7e21d281e2ab525cdd9b4e5b3aac
nacen <nacen.dev@gmail.com>
2022-04-25 23:46:28 +0800

    Use AllTodo in Sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { AllTodo } from "../features/ProjectList/AllTodo";
 import { ProjectList } from "../features/ProjectList/ProjectList";
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
-  
   return (
     <aside className="p-4 w-[200px] bg-slate-700">
+      <AllTodo />
       <h2 className="text-white text-2xl font-bold">Projects</h2>
       <ProjectList />
     </aside>
