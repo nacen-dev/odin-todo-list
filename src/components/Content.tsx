2dff4d136b208533c6ed6c3743845263feab8f8d
nacen <nacen.dev@gmail.com>
2022-04-25 23:47:04 +0800

    Add a check if selectContent is not null

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,21 +2,27 @@ import React from "react";
 import { useAppSelector } from "../app/hooks";
 import { selectActiveContent } from "../features/activeContent/activeContentSlice";
 import { Project } from "../features/ProjectList/Project";
+import { selectAllTodo } from "../features/ProjectList/projectListSlice";
 
 type Props = {};
 
 const Content = (props: Props) => {
   const activeContent = useAppSelector(selectActiveContent);
-  const selectProject = useAppSelector((state) => {
+  const selectContent = useAppSelector((state) => {
     let project = state.projectList.find(
       (project) => project.name === activeContent.id
     );
-    return project ? project : state.projectList[0];
+    return project;
   });
+
+  const allTodo = useAppSelector(selectAllTodo);
 
   return (
     <main className="bg-slate-600 h-screen p-4">
-      <Project name={selectProject.name} todoList={selectProject.todoList} />
+      {selectContent ? (
+        <Project name={selectContent.name} todoList={selectContent.todoList} />
+      ) : <Project name="" todoList={allTodo} />}
+      
     </main>
   );
 };
