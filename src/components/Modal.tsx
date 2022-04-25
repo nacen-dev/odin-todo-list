61fe4cfe0c3614e55dc4c11f28a0b36d879de424
nacen <nacen.dev@gmail.com>
2022-04-25 17:17:45 +0800

    Remove the close button from Modal
    It will up to the implementer of the modal to add a close button,
    but by default the modal will close if you click outside of the modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { MdClose } from "react-icons/md";
 
 type Props = {
   open: boolean;
@@ -14,10 +13,6 @@ const Modal = ({ children, open, onClose, className }: Props) => {
 
   let portalDiv = document.getElementById("portal");
 
-  const handleButtonClose = (event: React.MouseEvent<SVGElement>) => {
-    event.stopPropagation();
-    onClose();
-  };
 
   return portalDiv
     ? ReactDOM.createPortal(
@@ -27,12 +22,8 @@ const Modal = ({ children, open, onClose, className }: Props) => {
             onClick={onClose}
           />
           <div
-            className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5 z-[1000] ${className}`}
+            className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white z-[1000] rounded-md ${className}`}
           >
-            <MdClose
-              onClick={handleButtonClose}
-              className="absolute top-2 right-2 cursor-pointer text-3xl"
-            />
             {children}
           </div>
         </>,
