da2a6a8d57b00745bd510f1406ce62fec3379125
nacen <nacen.dev@gmail.com>
2022-04-26 20:05:07 +0800

    Add classname prop and make width to 100%

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { AllTodo } from "../features/ProjectList/AllTodo";
 import { ProjectList } from "../features/ProjectList/ProjectList";
 
-type Props = {};
+type Props = {
+  className?: string;
+};
 
-const Sidebar = (props: Props) => {
+const Sidebar = ({ className }: Props) => {
   return (
-    <aside className="p-4 w-[200px] bg-slate-700">
-      <AllTodo className="mb-2"/>
+    <aside className={`p-4 w-full bg-slate-700 ${className}`}>
+      <AllTodo className="mb-2" />
       <h2 className="text-white text-2xl font-bold">Projects</h2>
       <ProjectList />
     </aside>
